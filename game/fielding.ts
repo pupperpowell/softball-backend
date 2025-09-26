a1142f93ec4f30cbd20e6878c9d38a35bb6e9f0c
George Powell <geohpowell@gmail.com>
2025-09-25 23:44:15 -0400

    created fielding helper functions

--- game/fielding.ts
+++ game/fielding.ts
@@ -7,8 +7,22 @@
 // The ball is hit into the air!
 // A fielder must take action.
 
-import type { BattedBall } from "./types";
+import type { BattedBall, FieldingPosition } from "./types";
 
-export function simulateFielding() {
+export function simulateFielding(ball: BattedBall) {
+    
+}
 
+function calculateAirTime(ball: BattedBall): number {
+    // TODO: Calculate estimated air time based on BattedBall 
+    throw new Error("implement me");
+    return 0;
+}
+
+function estimateDropZone(ball: BattedBall): FieldingPosition {
+    // TODO: Figure out which position the ball will land near
+    // or, in the case of line drives, which positions the ball 
+    // will pass within reach of
+    throw new Error("implement me");
+    return "Bench"
 }
